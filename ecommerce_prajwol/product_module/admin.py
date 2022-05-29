a446ecba35804ffbc4ce92dbe733180c088cbb9c
PrajwolPradhan <prajwol.pradhan@apexcollege.edu.np>
2022-05-29 10:45:05 +0545

    new

--- ecommerce_prajwol/product_module/admin.py
+++ ecommerce_prajwol/product_module/admin.py
@@ -2,13 +2,27 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Brand, Category, Product
-admin.site.register(Brand)
-admin.site.register(Category)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ["name", "is_active",]
+    search_fields = ["name", "is_active",]
+    # readonly_fields = ["quantity",]
+
+    class Meta:
+        model = Brand
+admin.site.register(Brand, BrandAdmin)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "is_active",]
+    search_fields = ["name", "is_active",]
+    # readonly_fields = ["quantity",]
+
+    class Meta:
+        model = Category
+admin.site.register(Category, CategoryAdmin)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ["name", "price", "brand", "category",]
+    list_display = ["image_tag","name", "price", "brand", "category",]
     search_fields = ["name", "price", "brand__name", "category__name",]
-    list_filter = ["brand","category",]
-    readonly_fields = ["quantity",]
+    list_filter = ["brand","category","price",]
+    # readonly_fields = ["quantity",]
 
     class Meta:
         model = Product
