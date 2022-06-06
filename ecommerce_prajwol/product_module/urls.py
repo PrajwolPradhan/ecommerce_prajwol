14c437c38c1aafdda52a9a768f68f83dfd253c40
PrajwolPradhan <prajwol.pradhan@apexcollege.edu.np>
2022-06-06 16:03:32 +0545

    try

--- ecommerce_prajwol/product_module/urls.py
+++ ecommerce_prajwol/product_module/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index
+from .views import index, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('product_module.urls')),
